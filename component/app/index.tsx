294506bca51460414176a8bf71ee291605720f51
ajayakumar <ajayakumar@ADARSH>
2019-07-23 22:29:45 -0500

    Adding bootstrap

--- component/app/index.tsx
+++ component/app/index.tsx
@@ -1,14 +1,23 @@
 import {render} from 'react-dom';
 import * as React from 'react';
 
-class App extends React.Components<any,any>{
+class App extends React.Component<any,any>{
   constructor(props){
     super(props);
   }
 
   render(){
     return(
-      <div>Hello</div>
+      <div className="container custom-element">
+      <div className="form-group">
+        <input className="form-control"></input>
+      </div>
+      <div className="form-group">
+        <input className="form-control"></input>
+
+      </div>
+      <div className="form-group">        <button type="button" className="btn btn-primary">Primary</button></div>
+    </div>
     )
 }
 
--- hooks/app/index.tsx
+++ hooks/app/index.tsx
@@ -3,8 +3,15 @@ import * as React from 'react';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="container custom-element">
+      <div className="form-group">
+        <input className="form-control"></input>
+      </div>
+      <div className="form-group">
+        <input className="form-control"></input>
+
+      </div>
+      <div className="form-group">        <button type="button" className="btn btn-primary">Primary</button></div>
     </div>
   );
 }
