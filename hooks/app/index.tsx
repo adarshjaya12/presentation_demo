288dd64c596d43d6fbeab3dfb489280fb58a9da2
ajayakumar <ajayakumar@ADARSH>
2019-07-23 22:46:53 -0500

    Making updates to styling

--- hooks/app/index.tsx
+++ hooks/app/index.tsx
@@ -9,9 +9,10 @@ const App: React.FC = () => {
       </div>
       <div className="form-group">
         <input className="form-control"></input>
-
       </div>
-      <div className="form-group">        <button type="button" className="btn btn-primary">Primary</button></div>
+      <div className="form-group">        
+        <button type="button" className="btn btn-primary">Primary</button>
+      </div>
     </div>
   );
 }
